cfdcdf7fbcb12efc5ddd42dd2ecf4f3d092df46c
LAPTOP-CDBQ6EJA\academia <202607_aw_yonemori@x-ide.sakura.ne.jp>
2026-09-24 15:12:34 +0900

    レシピ文言追加コミット

---
+++ chapter_04/section_4_5/RecipeAi.java
@@ -0,0 +1,14 @@
+package chapter_04.section_4_5;
+
+/*
+ * カレーを作る手順を、4行のメッセージで画面に表示するプログラムです。
+ */
+public class RecipeAi {
+    public static void main(String[] args) {
+        // カレーを作る手順を順番に表示します。
+        System.out.println("野菜を切る");
+        System.out.println("肉と野菜を炒める"); // 肉と野菜を一緒に炒めます。
+        System.out.println("水を入れて煮込む");
+        System.out.println("ルウを溶かす");
+    }
+}
