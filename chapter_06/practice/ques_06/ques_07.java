faab6c102c6d0f598ddc464143219835cf4557c8
LAPTOP-CDBQ6EJA\academia <202607_aw_yonemori@x-ide.sakura.ne.jp>
2026-09-25 16:22:51 +0900

    お菓子の数をコミット

--- chapter_06/practice/ques_06/ques_07.java
+++ chapter_06/practice/ques_06/ques_07.java
@@ -2,5 +2,11 @@ package chapter_06.practice.ques_06;
 
 public class ques_07 {
     public static void main(String[] args) {
+        int okashi = 21;
+        int friend = 4;
+        int hitoriatari = okashi / friend;
+        int amari = okashi % friend;
+        System.out.println("一人当たりのお菓子の数" + hitoriatari + "つ");
+        System.out.println("余ったお菓子の数" + amari + "つ");
     }
 }
